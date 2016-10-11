4e884e6a8edb7c650cc21bcb9198e03b9fa9f948
Julian Møller <git@julianmoeller.dk>
2016-10-11 19:36:46 +0200

    Add index file

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export type { $Routes } from './Router';
+export { default as BrowserRouter } from './BrowserRouter';
+export { default as ServerRouter } from './ServerRouter';
+export { default as RouterMountpoint } from './RouterMountpoint';
+export { default as Fragment } from './Fragment';
+export { default as Link } from './Link';
