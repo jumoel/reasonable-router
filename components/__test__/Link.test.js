4992eeb667647de5dbef030af176d8cd20e67cda
Julian Møller <git@julianmoeller.dk>
2016-10-17 23:43:03 +0200

    Fix indentation

--- components/__test__/Link.test.js
+++ components/__test__/Link.test.js
@@ -4,31 +4,31 @@ import { shallow } from 'enzyme';
 import Link from '../Link';
 
 describe('Link', () => {
-it('renders with the proper text', () => {
-	const link = shallow(
-		<Link href='/'>Linktext</Link>
-	);
+	it('renders with the proper text', () => {
+		const link = shallow(
+			<Link href='/'>Linktext</Link>
+		);
 
-	expect(link.text()).toEqual('Linktext');
-});
+		expect(link.text()).toEqual('Linktext');
+	});
 
-it('renders with the proper `href` attribute', () => {
-	const link = shallow(
-		<Link href='/'>Linktext</Link>
-	);
+	it('renders with the proper `href` attribute', () => {
+		const link = shallow(
+			<Link href='/'>Linktext</Link>
+		);
 
-	expect(link.find({ href: '/' }).length).toEqual(1);
-});
+		expect(link.find({ href: '/' }).length).toEqual(1);
+	});
 
-it('renders with a named route', () => {
-	const getRoutes = () => ({ '/': { name: 'Frontpage' }});
+	it('renders with a named route', () => {
+		const getRoutes = () => ({ '/': { name: 'Frontpage' }});
 
-	const link = shallow(
-		<Link to='Frontpage'>Linktext</Link>,
-		{ context: { getRoutes } }
-	);
+		const link = shallow(
+			<Link to='Frontpage'>Linktext</Link>,
+			{ context: { getRoutes } }
+		);
 
-	expect(link.prop('href')).toEqual('/');
-	expect(link.text()).toEqual('Linktext');
-});
+		expect(link.prop('href')).toEqual('/');
+		expect(link.text()).toEqual('Linktext');
+	});
 });
