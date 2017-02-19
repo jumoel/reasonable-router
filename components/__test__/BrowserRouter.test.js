fd5053f3fb3888feef731d069239cb0d4fc53596
Julian Møller <git@julianmoeller.dk>
2017-02-19 15:31:47 +0100

    feat: Add tests for the two <Router /> wrappers
    Closes #16
    Closed #17

---
+++ components/__test__/BrowserRouter.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import BrowserRouter from '../BrowserRouter';
+import Router from '../Router';
+
+describe('<BrowserRouter />', () => {
+	const NotFound = () => <h1>Not Found</h1>;
+	const Page = () => <h1>Page</h1>;
+	const routeConfig = {
+		routes: {
+			'/': { component: Page },
+		},
+		miss: NotFound,
+	};
+	const location = '/?search#hash';
+
+	it('renders a <Route /> component', () => {
+		const result = shallow(<BrowserRouter location={location} routeConfig={routeConfig} />);
+
+		expect(result.type()).toEqual(Router);
+	});
+
+	it('passes along the routeConfig object', () => {
+		const result = shallow(<BrowserRouter routeConfig={routeConfig} />);
+
+		expect(result.node.props.routeConfig).toEqual(routeConfig);
+	});
+});
---
+++ components/__test__/ServerRouter.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ServerRouter from '../ServerRouter';
+import Router from '../Router';
+
+describe('<ServerRouter />', () => {
+	const NotFound = () => <h1>Not Found</h1>;
+	const Page = () => <h1>Page</h1>;
+	const routeConfig = {
+		routes: {
+			'/': { component: Page },
+		},
+		miss: NotFound,
+	};
+	const location = '/?search#hash';
+
+	it('renders a <Route /> component', () => {
+		const result = shallow(<ServerRouter location={location} routeConfig={routeConfig} />);
+
+		expect(result.type()).toEqual(Router);
+	});
+
+	it('passes along the routeConfig object', () => {
+		const result = shallow(<ServerRouter location={location} routeConfig={routeConfig} />);
+
+		expect(result.node.props.routeConfig).toEqual(routeConfig);
+	});
+
+	it('does not pass on the location property', () => {
+		const result = shallow(<ServerRouter location={location} routeConfig={routeConfig} />);
+
+		expect(result.node.props.location).toBeUndefined();
+	});
+
+	it('wraps the location in an history object', () => {
+		const result = shallow(<ServerRouter location={location} routeConfig={routeConfig} />);
+
+		expect(result.node.props.history).toBeDefined();
+		expect(result.node.props.history.location).toBeDefined();
+		expect(result.node.props.history.location.pathname).toEqual('/');
+		expect(result.node.props.history.location.search).toEqual('?search');
+		expect(result.node.props.history.location.hash).toEqual('#hash');
+	});
+});
